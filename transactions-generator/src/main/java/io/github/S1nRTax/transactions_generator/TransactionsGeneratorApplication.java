4dd6af3feeffc7cfe1cfa00e9b78ecbf610671d1
S1nRTax <badreddine.asaad4@gmail.com>
2025-10-12 10:48:11 +0100

    add: transactions generator service

---
+++ transactions-generator/src/main/java/io/github/S1nRTax/transactions_generator/TransactionsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.S1nRTax.transactions_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsGeneratorApplication.class, args);
+	}
+
+}
---
+++ transactions-generator/src/test/java/io/github/S1nRTax/transactions_generator/TransactionsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.S1nRTax.transactions_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
